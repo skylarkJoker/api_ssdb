ee4d9ea838e251c5733da1fb63f8f07056cd0e59
Joey <skylarknash@gmail.com>
2019-12-13 19:33:33 -0400

    update

--- config/config.js
+++ config/config.js
@@ -1,23 +1,29 @@
-var dotenv = require("dotenv");
-dotenv.config();
+require("dotenv").config();
+
 module.exports = {
   development: {
-    url: process.env.DATABASE_URL,
+    username: "",
+    password: "",
+    database: "",
+    host: "",
     dialect: "postgres",
     operatorsAliases: false,
     logging: false
   },
   test: {
-    username: "aardvark",
-    password: "skyl4rkjok3r",
-    database: "ssdb",
-    host: "127.0.0.1",
+    username: "",
+    password: "",
+    database: "",
+    host: "",
     dialect: "postgres",
     operatorsAliases: false,
     logging: false
   },
   production: {
-    url: process.env.DATABASE_URL,
+    username: "",
+    password: "",
+    database: "",
+    host: "",
     dialect: "postgres",
     operatorsAliases: false,
     logging: false
