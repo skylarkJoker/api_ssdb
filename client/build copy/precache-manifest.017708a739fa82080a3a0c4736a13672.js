0f84f3fab47131ab6047f1ea883a69cb940d4424
Joey <skylarknash@gmail.com>
2019-12-29 20:48:32 -0400

    updated client

---
+++ client/build copy/precache-manifest.017708a739fa82080a3a0c4736a13672.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03b78717ce265657f9bb16d1af0f3d00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "863943f807424a2679fa",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "442954289030c15815f0",
+    "url": "/static/css/main.876361c9.chunk.css"
+  },
+  {
+    "revision": "863943f807424a2679fa",
+    "url": "/static/js/2.f5248bd0.chunk.js"
+  },
+  {
+    "revision": "80468367949f3d8259c8396da9119225",
+    "url": "/static/js/2.f5248bd0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "442954289030c15815f0",
+    "url": "/static/js/main.83bdcae4.chunk.js"
+  },
+  {
+    "revision": "f2cc4df220a48345f8f2",
+    "url": "/static/js/runtime-main.006b8026.js"
+  },
+  {
+    "revision": "48ca8b505d1f714e4d9b05c51872a5bc",
+    "url": "/static/media/back.48ca8b50.svg"
+  },
+  {
+    "revision": "fbff8f05609595814403f1273379e740",
+    "url": "/static/media/back_white.fbff8f05.svg"
+  },
+  {
+    "revision": "c1ed482c06775bf330f9d8206855c925",
+    "url": "/static/media/file.c1ed482c.svg"
+  },
+  {
+    "revision": "24e6ce0387fbe3b434c655176e12523b",
+    "url": "/static/media/menu.24e6ce03.svg"
+  },
+  {
+    "revision": "be6e6a3aa75b91fe37a762e46f4de960",
+    "url": "/static/media/ssdb.be6e6a3a.svg"
+  }
+]);
